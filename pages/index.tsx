c0f93bfce2f2b5d3da0d039b3b1e9725ab4a1e1e
superdarkhorse0930@gmail.com <superdarkhorse0930@gmail.com>
2024-05-10 21:10:04 +0900

    Initial Commit - finished

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold ">
+        Welcome to react multi-range-slider tutorial
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
